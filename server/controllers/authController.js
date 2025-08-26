3754661bbc925da55740ea32e72ce4937b6c32cf
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-08-26 06:54:50 +0530

    Add email verification functionality to authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -125,3 +125,36 @@ export const sendVerifyOtp = async (req,res) => {
         res.json({success:false, message:error.message});
     }
 }
+
+//Verifying User's Account
+export const verifyEmail = async (req,res) => {
+    const {userId, otp} = req.body;
+
+    if(!userId || !otp){
+        return res.json({success:false, message:'Missing Details'});
+    }
+    try {
+        const user = await userModel.findById(userId);
+
+        if(!user){
+            return res.json({success:false, message:'User not found'});
+        }
+        if(user.verifyOtp == '' || user.verifyOtp !== otp){
+            return res.json({success:false, message:'Invalid OTP'});
+        }
+        if(user.verifyOtpExpiredAt < Date.now()){
+            return res.json({success:false, message:'OTP Expired'});
+        }
+        user.isAcountVerified = true;
+        user.verifyOtp = '';
+        user.verifyOtpExpiredAt = 0;
+
+        await user.save();
+
+        return res.json({success:true, message:'Email verified successfully'});
+
+    } catch (error) {
+        return res.json({success:false, message:error.message});
+    }
+
+}
